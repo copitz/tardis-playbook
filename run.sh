2d679c56674e5dee6d05dafc9f9a8c107d17620d
copitz <co@c12z.de>
2025-08-23 01:00:43 +0200

    Added run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+VENV=".venv"
+
+if [ ! -d "$VENV" ]; then
+  python3 -m venv "$VENV"
+fi
+
+source "$VENV/bin/activate"
+
+if ! python -c "import ansible" &>/dev/null; then
+  pip install --upgrade pip
+  pip install ansible
+fi
+
+ansible-playbook site.yml -i inventory
